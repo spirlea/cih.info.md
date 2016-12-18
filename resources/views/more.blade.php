3d46f3ebdef81f440088bf8137784ec6a51b9f73
iondo <iondo@DESKTOP-OIT5JMK>
2016-12-18 18:07:58 +0200

    events/more

--- resources/views/more.blade.php
+++ resources/views/more.blade.php
@@ -84,122 +84,13 @@
                     <a class="mail" href="#"><i class="fa fa-envelope"></i></a>
                   </div>
                 </div>
-                <div class="author-info clearfix">
-                  <div class="author-image">
-                    <a href="#"><img alt="" src="images/author.png" /></a>
-                  </div>
-                  <div class="author-bio">
-                    <h4>About The Author</h4>
-                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia desrut mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
-                  </div>
-                </div>
               </div>
               <!-- End Single Post Content -->
 
             </div>
             <!-- End Single Post Area -->
 @endif
-            <!-- Start Comment Area -->
-            <div id="comments">
-              <h2 class="comments-title">(4) Comments</h2>
-              <ol class="comments-list">
-                <li>
-                  <div class="comment-box clearfix">
-                    <div class="avatar"><img alt="" src="images/avatar.png" /></div>
-                    <div class="comment-content">
-                      <div class="comment-meta">
-                        <span class="comment-by">John Doe</span>
-                        <span class="comment-date">February 15, 2013 at 11:39 pm</span>
-                        <span class="reply-link"><a href="#">Reply</a></span>
-                      </div>
-                      <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia desrut mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
-                    </div>
-                  </div>
-
-                  <ul>
-                    <li>
-                      <div class="comment-box clearfix">
-                        <div class="avatar"><img alt="" src="images/avatar.png" /></div>
-                        <div class="comment-content">
-                          <div class="comment-meta">
-                            <span class="comment-by">John Doe</span>
-                            <span class="comment-date">February 15, 2013 at 11:39 pm</span>
-                            <span class="reply-link"><a href="#">Reply</a></span>
-                          </div>
-                          <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.</p>
-                        </div>
-                      </div>
-
-                      <ul>
-                        <li>
-                          <div class="comment-box clearfix">
-                            <div class="avatar"><img alt="" src="images/avatar.png" /></div>
-                            <div class="comment-content">
-                              <div class="comment-meta">
-                                <span class="comment-by">John Doe</span>
-                                <span class="comment-date">February 15, 2013 at 11:39 pm</span>
-                                <span class="reply-link"><a href="#">Reply</a></span>
-                              </div>
-                              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi.</p>
-                            </div>
-                          </div>
-                        </li>
-                      </ul>
-
-                    </li>
-                  </ul>
-                </li>
-
-                <li>
-                  <div class="comment-box clearfix">
-                    <div class="avatar"><img alt="" src="images/avatar.png" /></div>
-                    <div class="comment-content">
-                      <div class="comment-meta">
-                        <span class="comment-by">John Doe</span>
-                        <span class="comment-date">February 15, 2013 at 11:39 pm</span>
-                        <span class="reply-link"><a href="#">Reply</a></span>
-                      </div>
-                      <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia desrut mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
-                    </div>
-                  </div>
-                </li>
-
-              </ol>
-
-              <!-- Start Respond Form -->
-              <div id="respond">
-                <h2 class="respond-title">Leave a reply</h2>
-                <form action="#">
-                  <div class="row">
-                    <div class="col-md-4">
-                      <label for="author">Name<span class="required">*</span></label>
-                      <input id="author" name="author" type="text" value="" size="30" aria-required="true">
-                    </div>
-                    <div class="col-md-4">
-                      <label for="email">Email<span class="required">*</span></label>
-                      <input id="email" name="author" type="text" value="" size="30" aria-required="true">
-                    </div>
-                    <div class="col-md-4">
-                      <label for="url">Website<span class="required">*</span></label>
-                      <input id="url" name="url" type="text" value="" size="30" aria-required="true">
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-md-12">
-                      <label for="comment">Add Your Comment</label>
-                      <textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea>
-                      <input name="submit" type="submit" id="submit" value="Submit Comment">
-                    </div>
-                  </div>
-                </form>
-              </div>
-              <!-- End Respond Form -->
-
-            </div>
-            <!-- End Comment Area -->
-
           </div>
-
 
           <!-- Sidebar -->
           <div class="col-md-3 sidebar right-sidebar">
@@ -268,30 +159,7 @@
               </ul>
             </div>
 
-            <!-- Video Widget -->
-            <div class="widget">
-              <h4>Video <span class="head-line"></span></h4>
-              <div>
-                <iframe src="http://player.vimeo.com/video/63322694?byline=0&amp;portrait=0&amp;badge=0" width="800" height="450"></iframe>
-              </div>
-            </div>
-
-            <!-- Tags Widget -->
-            <div class="widget widget-tags">
-              <h4>Tags <span class="head-line"></span></h4>
-              <div class="tagcloud">
-                <a href="#">Portfolio</a>
-                <a href="#">Theme</a>
-                <a href="#">Mobile</a>
-                <a href="#">Design</a>
-                <a href="#">Wordpress</a>
-                <a href="#">Jquery</a>
-                <a href="#">CSS</a>
-                <a href="#">Modern</a>
-                <a href="#">Theme</a>
-                <a href="#">Icons</a>
-                <a href="#">Google</a>
-              </div>
+           
             </div>
 
           </div>
