5a9d6c17be6e708145dce7df465ff7d1f7f4bb90
iondo <iondo@DESKTOP-OIT5JMK>
2016-12-18 19:25:36 +0200

    sidebar

---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,68 @@
+<!-- Sidebar -->
+          <div class="col-md-3 sidebar right-sidebar">
+
+            <!-- Search Widget -->
+            <div class="widget widget-search">
+              <form action="#">
+                <input type="search" placeholder="Enter Keywords..." />
+                <button class="search-btn" type="submit"><i class="fa fa-search"></i></button>
+              </form>
+            </div>
+
+            <!-- Categories Widget -->
+            <div class="widget widget-categories">
+              <h4>Categories <span class="head-line"></span></h4>
+              <ul>
+                <li>
+                  <a href="#">Brandign</a>
+                </li>
+                <li>
+                  <a href="#">Design</a>
+                </li>
+                <li>
+                  <a href="#">Development</a>
+                </li>
+                <li>
+                  <a href="#">Graphic</a>
+                </li>
+              </ul>
+            </div>
+
+            <!-- Popular Posts widget -->
+            <div class="widget widget-popular-posts">
+              <h4>Popular Post <span class="head-line"></span></h4>
+              <ul>
+                <li>
+                  <div class="widget-thumb">
+                    <a href="#"><img src="images/blog-mini-01.jpg" alt="" /></a>
+                  </div>
+                  <div class="widget-content">
+                    <h5><a href="#">How To Download The Google Fonts Catalog</a></h5>
+                    <span>Jul 29 2013</span>
+                  </div>
+                  <div class="clearfix"></div>
+                </li>
+                <li>
+                  <div class="widget-thumb">
+                    <a href="#"><img src="images/blog-mini-02.jpg" alt="" /></a>
+                  </div>
+                  <div class="widget-content">
+                    <h5><a href="#">How To Download The Google Fonts Catalog</a></h5>
+                    <span>Jul 29 2013</span>
+                  </div>
+                  <div class="clearfix"></div>
+                </li>
+                <li>
+                  <div class="widget-thumb">
+                    <a href="#"><img src="images/blog-mini-03.jpg" alt="" /></a>
+                  </div>
+                  <div class="widget-content">
+                    <h5><a href="#">How To Download The Google Fonts Catalog</a></h5>
+                    <span>Jul 29 2013</span>
+                  </div>
+                  <div class="clearfix"></div>
+                </li>
+              </ul>
+            </div>
+        </div>
+          <!--End sidebar-->
